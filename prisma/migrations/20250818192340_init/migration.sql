52d118104cd4f257b38aeedb03459eda1a8b36a6
tonyissa96 <52683702+tonyissa@users.noreply.github.com>
2025-08-18 15:35:41 -0400

    Initial commit

---
+++ prisma/migrations/20250818192340_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
